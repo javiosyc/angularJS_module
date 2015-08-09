2c05f18d15fb8079651ba87902d63ed23df27e4e
javiosyc <javiosyc@gmail.com>
2015-08-09 12:32:52 +0800

    angularJs module

---
+++ main.js
@@ -0,0 +1,10 @@
+var app = angular.module('app', ['ngCookies'])
+    //.controller('MainCtrl', MainCtrl);
+    
+  //  .run(function(version, $rootScope) {
+  //      $rootScope.version = version;
+  //  })
+;
+
+
+
---
+++ values.js
@@ -0,0 +1,4 @@
+(function() {
+    angular.module('app')
+        .value('version', '1.0.0');
+}());
